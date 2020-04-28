f6139d67adeedb4c3e2e7a9a8d2ee057a2c43212
unknown <bsse1017@iit.du.ac.bd>
2020-04-29 05:28:34 +0600

    radowan commiting

---
+++ lib/cloudFireStore/crudFireStore.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FireBaseUserMethods {
+
+  Future<void> addWords(List<String> word){
+      Firestore.instance.collection("wordlist").document("1").updateData({
+        'words' : FieldValue.arrayUnion(word)
+      });
+  }
+
+  Future<void> getUsersAllWordlist(){
+
+  }
+
+  Future<void> getParticularUserWordlist(){
+
+  }
+
+  Future<void> getAllPublicWordlist(){
+    
+  }
+
+
+}
